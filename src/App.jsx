3303bb6a8b85dcdef481bf78a19c9d37ce3fbdce
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-08-25 21:14:55 +0200

    Project setup done

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.scss'
+
+
+// import reactLogo from './assets/images/react.svg'
+function App() {
+
+  return (
+    <>
+
+
+
+      {/* <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <button>count is</button>
+      <p>
+        Edit <code>src/App.jsx</code> save to test
+      </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
